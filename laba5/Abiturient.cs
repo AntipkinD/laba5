5f41bc5d202afd51b8689d9c7f27ebd4f6ec8b6c
mushroomvoodoo <normeeboy@gmail.com>
2023-02-26 21:01:58 +0300

    laba6moment

--- laba5/Abiturient.cs
+++ laba5/Abiturient.cs
@@ -1,8 +1,10 @@
-﻿public class Abiturient
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Linq;
+public class Abiturient
 {
     public string asurname, bname, csecondname, eadres;
-    public int oz1, oz2, oz3, oz4, oz5;
-public Abiturient() 
+    public int oz1, oz2, oz3, oz4, oz5, summozenok;
+public Abiturient() //конструктор по умолчанию
         {
         this.asurname = "unknown";
         this.bname = "unknown";
@@ -13,8 +15,9 @@ public Abiturient()
         this.oz3 = 0;
         this.oz4 = 0;
         this.oz5 = 0;
+        this.summozenok = this.oz1 * 1 + this.oz2 * 2 + this.oz3 * 3 + this.oz4 * 4 + this.oz5*5;
         }
-public Abiturient(string surname, string name, string secondname, string adres, int oz1, int oz2, int oz3, int oz4, int oz5)
+public Abiturient(string surname, string name, string secondname, string adres, int oz1, int oz2, int oz3, int oz4, int oz5) //конструктор с параметрами
     {
         this.asurname = surname;
         this.bname = name;
@@ -25,8 +28,17 @@ public Abiturient(string surname, string name, string secondname, string adres, 
         this.oz3 = oz3;
         this.oz4 = oz4;
         this.oz5 = oz5;
+        this.summozenok = this.oz1 * 1 + this.oz2 * 2 + this.oz3 * 3 + this.oz4 * 4 + this.oz5 * 5;
     }
-public static void ProverkaA(Abiturient[] value)
+~Abiturient() //деструктор
+    {   
+        Console.WriteLine($"Объект {this.asurname} уничтожен");
+    }
+public static void objectGenerator(string surname, string name, string secondname, string adres, int oz1, int oz2, int oz3, int oz4, int oz5) //создатель объектов
+    {
+        Abiturient abi = new Abiturient(surname, name, secondname, adres, oz1, oz2, oz3, oz4, oz5);
+    }
+    public static void ProverkaA(Abiturient[] value)
     {
         Console.WriteLine("Неудовлетворительные оценки имеют следующие студенты");
         for (int j = 0; j < value.Length; j++)
@@ -47,5 +59,15 @@ public static void ProverkaB(Abiturient[] value)
                 Console.WriteLine($"{value[j].asurname} {value[j].bname} {value[j].csecondname}, сумма оценок: {value[j].oz1 * 1 + value[j].oz2 * 2 + value[j].oz3 * 3 + value[j].oz4 * 4 + value[j].oz5 * 5}");
             }
         Console.WriteLine("______________________________________________________________________________________________________________________");
+    }
+public static void ProverkaC(Abiturient[] value)
+    {
+        var provc = value.OrderByDescending(ob => ob.summozenok).ToArray();
+        Array.ForEach(provc, Console.WriteLine);
+
+    }
+    public override string ToString()
+    {
+        return $"{asurname} {bname} {csecondname} с суммой оценок {summozenok}";
     }
 }
--- laba5/Program.cs
+++ laba5/Program.cs
@@ -1,35 +1,32 @@
-﻿Abiturient[] arra = new Abiturient[8];
-Abiturient Artem = new Abiturient("Костенко", "Виктор", "Владиславович", "ул. Бекешская 16", 4, 5, 4, 2, 1);
-Abiturient Vlad = new Abiturient("Костенко", "Владислав", "Владиславович", "ул. Бекешская 16", 0, 0, 4, 2, 6);
-Abiturient Sergey = new Abiturient("Медведчук", "Сергей", "Андреевич", "ул. Пушкина 12", 1, 0, 2, 3, 5);
-Abiturient Shinji = new Abiturient("Икари", "Синдзи", "Геннадьевич", "ул. Фудзимото 1", 0, 0, 5, 1, 1);
-Abiturient NoName = new Abiturient();
-Abiturient Victor = new Abiturient("Баринов", "Виктор", "Петрович", "ул. Фритату 2", 15, 10, 1, 0, 1);
-Abiturient Kolya = new Abiturient("Барискин", "Олег", "Викторович", "ул. Ветеранов 12", 0, 0, 0, 3, 15);
-Abiturient Sasha = new Abiturient("Верщенко", "Анатолий", "Григорьевич", "ул. Бекешская 23", 0, 0, 4, 2, 6);
-arra[0] = NoName;
-arra[1] = Vlad;
-arra[2] = Sergey;
-arra[3] = Shinji;
-arra[4] = Artem;
-arra[5] = Victor;
-arra[6] = Kolya;
-arra[7] = Sasha;
-
-Abiturient.ProverkaA(arra);
-Abiturient.ProverkaB(arra);
-ProverkaC(arra);
-void ProverkaC(Abiturient[] value)
+﻿internal class Program
 {
-    int m1 = 0;
-    int m2 = 0;
-    int m3 = 0;
-    for (int i = 0; i < value.Length; i++)
+    private static void Main(string[] args)
     {
-        for (int j = 1; i < value.Length; i++)
+        int n = int.Parse(Console.ReadLine());
+        Abiturient[] arra =
         {
-            
+new Abiturient("Костенко", "Виктор", "Владиславович", "ул. Бекешская 16", 4, 5, 4, 2, 1),
+new Abiturient("Костенко", "Владислав", "Владиславович", "ул. Бекешская 16", 0, 0, 4, 2, 6),
+new Abiturient("Медведчук", "Сергей", "Андреевич", "ул. Пушкина 12", 0, 0, 2, 3, 5),
+new Abiturient("Икари", "Синдзи", "Геннадьевич", "ул. Фудзимото 1", 0, 0, 5, 1, 1),
+new Abiturient(),
+new Abiturient("Баринов", "Виктор", "Петрович", "ул. Фритату 2", 15, 10, 1, 0, 1),
+new Abiturient("Барискин", "Олег", "Викторович", "ул. Ветеранов 12", 0, 0, 0, 3, 15),
+new Abiturient("Верщенко", "Анатолий", "Григорьевич", "ул. Бекешская 23", 0, 0, 4, 2, 6),
+};
+
+        /*for (int c = 8; c<arra.Length; c++)
+        {
+            arra[c] = new Abiturient(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
         }
+        */
+        for (int i = 0; i < 120000; i++)
+            Abiturient.objectGenerator($"Musor {i}", null, null, null, 0, 0, 0, 0, 0);
+        Abiturient.ProverkaC(arra);
+        Abiturient.ProverkaA(arra);
+        Abiturient.ProverkaB(arra);
+        
+
         
     }
 }
